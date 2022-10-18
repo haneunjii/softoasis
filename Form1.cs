912a564fcb4aecefdcaeb867ecf5b75781f8cd65
haneunjii <106488799+haneunjii@users.noreply.github.com>
2022-10-19 05:22:31 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+
+namespace supermarket
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        Display.login_page lg=new Display.login_page();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Controls.Add(lg);
+        }
+        
+        
+    }
+}
---
+++ upload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket
+{
+    internal class upload
+    {
+        public string Name { get; set; }    //Name이란 변수를 스트링 형태로 선언
+        public string id { get; set; }    //id이란 변수를 스트링 형태로 선언
+        public string pw { get; set; }   //pw이란 변수를 스트링 형태로 선언
+    }
+}
